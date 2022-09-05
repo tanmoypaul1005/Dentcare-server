efed7efaee5acf265536075c04b3e02e2375b8ed
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2022-09-05 22:08:54 +0600

    ffff

--- src/Model/DentalCheckup.js
+++ src/Model/DentalCheckup.js
@@ -1,5 +1,4 @@
 const { default: mongoose } = require("mongoose");
-const mongooseSerial = require("mongoose-serial")
 const DentalCheckupSchema=new mongoose.Schema({
     name:{type:String},
     price:{type:String},
