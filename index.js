ca83e7d726d1b44396ffa19988bda61eb3b1f12b
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2022-09-05 13:36:26 +0600

    kk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
-
+const app = express();
+const port=process.env.PORT
 require('dotenv').config();
 const DataBaseconnect = require("./src/config/DataBase");
 
@@ -10,11 +10,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.json());
 
-
-
 //DataBase Connectd
 DataBaseconnect()
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -31,6 +28,6 @@ app.use('/api',DoctorRoutes);
 app.use('/api',DentalCheckupRoutes);
 app.use('/api',CheckupAppointmentRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
